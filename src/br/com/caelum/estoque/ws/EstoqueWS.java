9a348a7234dc2417f749cd5b33466c9680e9424a
Bruna Silva <bruna-m.silva@hotmail.com>
2020-06-04 13:37:22 -0300

    Alterações 03-06-2020
    Alteração da classe EstoqueWs e criação da classe AutorizacaoException.

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -10,6 +10,8 @@ import javax.xml.ws.ResponseWrapper;
 import java.util.List;
 
 import br.com.caelum.estoque.modelo.item.*;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 
 @WebService
@@ -17,15 +19,30 @@ public class EstoqueWS {
 
     private ItemDao dao = new ItemDao();
 
-    @WebMethod(operationName="todosOsItens")
-    @ResponseWrapper(localName="itens")
-    @WebResult(name="item")
-    @RequestWrapper(localName="listaItens")
+    @WebMethod(operationName = "todosOsItens")
+    @ResponseWrapper(localName = "itens")
+    @WebResult(name = "item")
+    @RequestWrapper(localName = "listaItens")
     public List<Item> getItens(@WebParam(name = "filtros") Filtros filtros) {
 
         List<Filtro> lista = filtros.getLista();
         List<Item> result = dao.todosItens(lista);
 
         return result;
+    }
+
+    @WebMethod(operationName = "cadastrarItens")
+    @WebResult(name = "item")
+    public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
+                              @WebParam(name = "item") Item item) throws AutorizacaoException {
+
+        System.out.println("Cadastrando " + item + ", " + token);
+        boolean valido = new TokenDao().ehValido(token);
+        if (!valido) {
+            throw new AutorizacaoException("Token invalido");
+        }
+
+        this.dao.cadastrar(item);
+        return item;
     }
 }
